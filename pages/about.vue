115ee58fd330e481e4b9125f0e154abced380dd8
Harun1804 <Harun.arrasyid1804@gmail.com>
2022-02-17 10:19:46 +0700

    Initial

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/about">About</nuxt-link>
+
+        <h1>Ini About</h1>
+    </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+<main>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/about">About</nuxt-link>
+
+    <h1>Ini Main</h1>
+</main>
+</template>
